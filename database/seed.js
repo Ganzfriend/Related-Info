23f18935d7a9202ecd466051ac8627030d35a92e
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-01-12 20:39:52 -0700

    Add more dummy data

--- database/seed.js
+++ database/seed.js
@@ -4,7 +4,7 @@
 const City = require('./index.js');
 
 const sampleCities = [
-{ name: "Seattle",
+{ name: "Seattle, WA",
   data: {
     homes:
     [ {
@@ -12,7 +12,7 @@ const sampleCities = [
       reviews: 8,
       type: "Private room",
       beds: 2,
-      description: "Cozy and quaint, but also haunted",
+      description: "Cozy and quaint, but also haunted (by Elon Musk's time-travelling ghost)",
       price: 65,
       superhost: true,
       liked: true,
@@ -34,6 +34,125 @@ const sampleCities = [
         name: "Olympia, WA",
         url: "https://www.google.com"
     } ]
+  } },
+
+  { name: "Oakland, CA",
+  data: {
+    homes:
+    [ {
+      image: "https://unsplash.com/photos/ezRKQQPWJOw",
+      reviews: 8,
+      type: "Entire house",
+      beds: 4,
+      description: "It's definitely a house. Come check it out!",
+      price: 235,
+      superhost: false,
+      liked: false,
+      location: "Oakland, CA"
+    } ] ,
+
+    activities:
+    [ {
+      image: "https://unsplash.com/photos/JDSjHhdgpZY",
+      reviews: 1,
+      description: "Come check out this brown wooden dock. A favorite of Oakland locals!",
+      price: 765,
+      liked: false,
+      location: "Deep in the woods, by the water."
+    } ] ,
+
+    nearbyCities:
+    [ {
+        name: "San Jose, CA",
+        url: "https://www.google.com"
+      },
+      {
+        name: "San Francisco, CA",
+        url: "https://www.google.com"
+      }
+   ]
+  } },
+
+  { name: "Hollywood, CA",
+  data: {
+    homes:
+    [ {
+      image: "https://unsplash.com/photos/To7GxHUMq6g",
+      reviews: 8,
+      type: "Shared room",
+      beds: 1,
+      description: "The rats don't bite, as long as you're awake.",
+      price: 115,
+      superhost: false,
+      liked: true,
+      location: "Studio City, CA"
+    } ] ,
+
+    activities:
+    [ {
+      image: "https://unsplash.com/photos/8LlJNFLTEm0",
+      reviews: 25,
+      description: "Celebrities nowhere. Tourists everywhere. You're gonna love this tour!",
+      price: 125,
+      liked: true,
+      location: "West Hollywood, CA"
+    } ] ,
+
+    nearbyCities:
+    [ {
+        name: "Santa Monica, CA",
+        url: "https://www.google.com"
+      },
+      {
+        name: "Los Angeles, CA",
+        url: "https://www.google.com"
+      },
+      {
+        name: "Pasadena, CA",
+        url: "https://www.google.com"
+      },
+      {
+        name: "El Segundo, CA",
+        url: "https://www.google.com"
+      }
+    ]
+  } },
+
+  { name: "Austin, TX",
+  data: {
+    homes:
+    [ {
+      image: "https://unsplash.com/photos/E9Udq-cPVXs",
+      reviews: 0,
+      type: "Living room only",
+      beds: 0,
+      description: "The best Des. Cap. experience you'll ever have",
+      price: 18,
+      superhost: false,
+      liked: false,
+      location: "Taylor, TX"
+    } ] ,
+
+    activities:
+    [ {
+      image: "https://unsplash.com/photos/HFbzqwfnrsE",
+      reviews: 68,
+      description: "Paddleboarding lessons! Just don't fall in!",
+      price: 70,
+      liked: true,
+      location: "Butler Shores at Town Lake Metro Park Trail, Austin, TX"
+    } ] ,
+
+    nearbyCities:
+    [ {
+        name: "Jollyville, TX",
+        url: "https://www.google.com"
+      },
+      {
+        name: "Round Rock, TX",
+        url: "https://www.google.com"
+      }
+    ]
   } }
 ];
 
