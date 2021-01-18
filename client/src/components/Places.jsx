eec3ace7ef57c6697b94398f2e489af9ce6cc4d1
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-01-18 15:34:15 -0700

    add file/url loaders, carousel renders huge

--- client/src/components/Places.jsx
+++ client/src/components/Places.jsx
@@ -12,8 +12,8 @@ import { makeStyles } from '@material-ui/core/styles';
 // import Carousel from 'react-material-ui-carousel';
 // import Box from '@material-ui/core/Box';
 import Slider from 'react-slick';
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import HomeCard from './HomeCard';
 import styles from '../styles.js';
 
