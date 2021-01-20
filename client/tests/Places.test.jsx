621f1c57c82adfb44efc5272b04f80810c2066f3
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-01-20 12:34:42 -0700

    trying to write tests

--- client/tests/Places.test.jsx
+++ client/tests/Places.test.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // __tests__/Places.test.js
 /**
  * @test-environment jsdom
@@ -7,36 +8,38 @@ import React from 'react';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { waitFor, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+// import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent } from './test-utils';
 import Places from '../src/components/Places';
 
+const city = 'Seattle, WA';
+
 const server = setupServer(
-  rest.get('/homes', (req, res, ctx) => res(ctx.json({ greeting: 'hello there' }))),
+  rest.get(`/homes/${city}`, (req, res, ctx) => res(ctx.json({ greeting: 'hello there' }))),
 );
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-test('loads and displays greeting', async () => {
-  render(<Places url="/greeting" />);
+test('loads and displays home cards', async () => {
+  render(<Places />);
 
-  fireEvent.click(screen.getByText('Load Greeting'));
+  // fireEvent.click(screen.getByText('Load Greeting'));
 
-  await waitFor(() => screen.getByRole('heading'));
+  // await waitFor(() => screen.getByRole('heading'));
 
-  expect(screen.getByRole('heading')).toHaveTextContent('hello there');
+  expect(screen.getByRole('heading')).toHaveTextContent('Loading...');
   expect(screen.getByRole('button')).toHaveAttribute('disabled');
 });
 
 test('handles server error', async () => {
   server.use(
-    rest.get('/greeting', (req, res, ctx) => res(ctx.status(500))),
+    rest.get(`/homes/${city}`, (req, res, ctx) => res(ctx.status(500))),
   );
 
-  render(<Places url="/greeting" />);
+  render(<Places />);
 
   fireEvent.click(screen.getByText('Load Greeting'));
 
@@ -46,66 +49,63 @@ test('handles server error', async () => {
   expect(screen.getByRole('button')).not.toHaveAttribute('disabled');
 });
 
-////////////////////////////////////////////////////////////
+/// /////////////////////////////////////////////////////////
 
-const server = setupServer(
-  rest.post('/login', (req, res, ctx) => {
-    // Respond with a mocked user token that gets persisted
-    // in the `sessionStorage` by the `Login` component.
-    return res(ctx.json({ token: 'mocked_user_token' }))
-  }),
-)
+// // Enable API mocking before tests.
+// beforeAll(() => server.listen())
 
-// Enable API mocking before tests.
-beforeAll(() => server.listen())
+// // Reset any runtime request handlers we may add during the tests.
+// afterEach(() => server.resetHandlers())
 
-// Reset any runtime request handlers we may add during the tests.
-afterEach(() => server.resetHandlers())
-
-// Disable API mocking after the tests are done.
-afterAll(() => server.close())
+// // Disable API mocking after the tests are done.
+// afterAll(() => server.close())
 
 test('allows the user to log in', async () => {
-  render(<Login />)
+  server.use(
+    // Respond with a mocked user token that gets persisted
+    // in the `sessionStorage` by the `Login` component.
+    rest.post('/login', (req, res, ctx) => res(ctx.json({ token: 'mocked_user_token' }))),
+  );
+
+  render(<Places />);
   userEvent.type(
     screen.getByRole('textbox', { name: /username/i }),
     'john.maverick',
-  )
+  );
   userEvent.type(
     screen.getByRole('textbox', { name: /password/i }),
     'super-secret',
-  )
-  userEvent.click(screen.getByText(/submit/i))
-  const alert = await screen.findByRole('alert')
+  );
+  userEvent.click(screen.getByText(/submit/i));
+  const alert = await screen.findByRole('alert');
 
   // Assert successful login state
-  expect(alert).toHaveTextContent(/welcome/i)
-  expect(window.sessionStorage.getItem('token')).toEqual(fakeUserResponse.token)
-})
+  expect(alert).toHaveTextContent(/welcome/i);
+  expect(window.sessionStorage.getItem('token')).toEqual(fakeUserResponse.token);
+});
 
 test('handles login exception', () => {
   server.use(
-    rest.post('/login', (req, res, ctx) => {
+    rest.post('/login', (req, res, ctx) =>
       // Respond with "500 Internal Server Error" status for this test.
-      return res(
+      res(
         ctx.status(500),
         ctx.json({ message: 'Internal Server Error' }),
-      )
-    }),
-  )
+      )),
+  );
 
-  render(<Login />)
+  render(<Login />);
   userEvent.type(
     screen.getByRole('textbox', { name: /username/i }),
     'john.maverick',
-  )
+  );
   userEvent.type(
     screen.getByRole('textbox', { name: /password/i }),
     'super-secret',
-  )
-  userEvent.click(screen.getByText(/submit/i))
+  );
+  userEvent.click(screen.getByText(/submit/i));
 
   // Assert meaningful error message shown to the user
-  expect(alert).toHaveTextContent(/sorry, something went wrong/i)
-  expect(window.sessionStorage.getItem('token')).toBeNull()
-})
+  expect(alert).toHaveTextContent(/sorry, something went wrong/i);
+  expect(window.sessionStorage.getItem('token')).toBeNull();
+});
