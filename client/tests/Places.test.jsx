25c3618cd2468dd15bfb88e9d0f2bdd5a959dd6c
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-01-20 12:15:33 -0700

    config for jest / react testing library

--- client/tests/Places.test.jsx
+++ client/tests/Places.test.jsx
@@ -7,9 +7,10 @@ import React from 'react';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { waitFor, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
-import Places from '../src/components/Places';
 import { render, fireEvent } from './test-utils';
+import Places from '../src/components/Places';
 
 const server = setupServer(
   rest.get('/homes', (req, res, ctx) => res(ctx.json({ greeting: 'hello there' }))),
@@ -44,3 +45,67 @@ test('handles server error', async () => {
   expect(screen.getByRole('alert')).toHaveTextContent('Oops, failed to fetch!');
   expect(screen.getByRole('button')).not.toHaveAttribute('disabled');
 });
+
+////////////////////////////////////////////////////////////
+
+const server = setupServer(
+  rest.post('/login', (req, res, ctx) => {
+    // Respond with a mocked user token that gets persisted
+    // in the `sessionStorage` by the `Login` component.
+    return res(ctx.json({ token: 'mocked_user_token' }))
+  }),
+)
+
+// Enable API mocking before tests.
+beforeAll(() => server.listen())
+
+// Reset any runtime request handlers we may add during the tests.
+afterEach(() => server.resetHandlers())
+
+// Disable API mocking after the tests are done.
+afterAll(() => server.close())
+
+test('allows the user to log in', async () => {
+  render(<Login />)
+  userEvent.type(
+    screen.getByRole('textbox', { name: /username/i }),
+    'john.maverick',
+  )
+  userEvent.type(
+    screen.getByRole('textbox', { name: /password/i }),
+    'super-secret',
+  )
+  userEvent.click(screen.getByText(/submit/i))
+  const alert = await screen.findByRole('alert')
+
+  // Assert successful login state
+  expect(alert).toHaveTextContent(/welcome/i)
+  expect(window.sessionStorage.getItem('token')).toEqual(fakeUserResponse.token)
+})
+
+test('handles login exception', () => {
+  server.use(
+    rest.post('/login', (req, res, ctx) => {
+      // Respond with "500 Internal Server Error" status for this test.
+      return res(
+        ctx.status(500),
+        ctx.json({ message: 'Internal Server Error' }),
+      )
+    }),
+  )
+
+  render(<Login />)
+  userEvent.type(
+    screen.getByRole('textbox', { name: /username/i }),
+    'john.maverick',
+  )
+  userEvent.type(
+    screen.getByRole('textbox', { name: /password/i }),
+    'super-secret',
+  )
+  userEvent.click(screen.getByText(/submit/i))
+
+  // Assert meaningful error message shown to the user
+  expect(alert).toHaveTextContent(/sorry, something went wrong/i)
+  expect(window.sessionStorage.getItem('token')).toBeNull()
+})
--- client/tests/test-utils.jsx
+++ client/tests/test-utils.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {
+  render, queries, queryHelpers, buildQueries,
+} from '@testing-library/react';
 import { ThemeProvider } from 'my-ui-lib';
 import { TranslationProvider } from 'my-i18n-lib';
 import defaultStrings from 'i18n/en-x-default';
+import * as customQueries from './custom-queries';
 
 const AllTheProviders = ({ children }) => (
   <ThemeProvider theme="light">
@@ -13,6 +16,37 @@ const AllTheProviders = ({ children }) => (
 );
 
 const customRender = (ui, options) => render(ui, { wrapper: AllTheProviders, ...options });
+
+/// ///////////////////////////////////////////////
+// The queryAllByAttribute is a shortcut for attribute-based matchers
+// You can also use document.querySelector or a combination of existing
+// testing library utilities to find matching nodes for your query
+const queryAllByDataCy = (...args) => queryHelpers.queryAllByAttribute('data-cy', ...args);
+
+const getMultipleError = (c, dataCyValue) => `Found multiple elements with the data-cy attribute of: ${dataCyValue}`;
+const getMissingError = (c, dataCyValue) => `Unable to find an element with the data-cy attribute of: ${dataCyValue}`;
+
+const [
+  queryByDataCy,
+  getAllByDataCy,
+  getByDataCy,
+  findAllByDataCy,
+  findByDataCy,
+] = buildQueries(queryAllByDataCy, getMultipleError, getMissingError);
+
+export {
+  queryByDataCy,
+  queryAllByDataCy,
+  getByDataCy,
+  getAllByDataCy,
+  findAllByDataCy,
+  findByDataCy,
+};
+
+const { getByDataCy } = render(<Component />)
+
+expect(getByDataCy('my-component')).toHaveTextContent('Hello')
+/// ////////////////////////////////////////
 
 // re-export everything
 export * from '@testing-library/react';
