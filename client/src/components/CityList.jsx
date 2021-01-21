37af2e3eab776c4093c00d8aa46be4bb850906d1
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-01-21 12:56:27 -0700

    cityList & skeletons done, update state not working

---
+++ client/src/components/CityList.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable import/extensions */
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import styles from '../styles.js';
+
+const useStyles = makeStyles(styles);
+
+const CityList = ({ cities, setCity }) => {
+  const classes = useStyles();
+  // const preventDefault = (event) => event.preventDefault();
+  return (
+    <Box className={classes.cityList}>
+      { cities.map((city) => {
+        const url = `/homes/${city.name}`;
+        return (
+          <Typography key={city._id} className={classes.cities}>
+            <Button href="#" onClick={() => { setCity(city.name); }}>
+              {city.name}
+            </Button>
+          </Typography>
+
+        );
+      })}
+    </Box>
+  );
+};
+
+export default CityList;
