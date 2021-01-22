9e6d5e814f17f57e08205180d280646b9426bcbe
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-01-22 15:09:53 -0700

    update id name in html

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 // import './styles.js';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app4'));
 
 // this file will eventually be abstracted out to the proxy server
