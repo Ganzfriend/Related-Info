bc0fb77ecb4f9d3e27cd1fd2ac5906cfd2dafbbe
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-01-22 18:13:25 -0700

    serving to proxy successfully!

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,7 @@
+/* eslint-disable import/extensions */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-// import './styles.js';
+import './styles.js';
 
 ReactDOM.render(<App />, document.getElementById('app4'));
-
-// this file will eventually be abstracted out to the proxy server
